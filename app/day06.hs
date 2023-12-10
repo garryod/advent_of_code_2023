ad7ea46a3a53fb21d118b4d2c9d8a45e5e568121
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2023-12-10 22:03:09 +0000

    Day6 Part2

--- app/day06.hs
+++ app/day06.hs
@@ -9,6 +9,7 @@ main = do
   handle <- openFile "data/day06.txt" ReadMode
   contents <- hGetContents handle
   print (product $ map (length . winningHoldDurations) (readRaces contents))
+  print (length $ winningHoldDurations $ readBigRace contents)
   hClose handle
 
 winningHoldDurations :: Race -> [Int]
@@ -18,6 +19,15 @@ winningHoldDurations race = filter recordBreaking [0 .. duration race]
 
 distanceTravelled :: Int -> Int -> Int
 distanceTravelled raceDuration holdDuration = holdDuration * (raceDuration - holdDuration)
+
+readBigRace :: String -> Race
+readBigRace input = Race time distance
+  where
+    [timesRow, distancesRow] = take 2 $ lines input
+    (_ : timesWords) = words timesRow
+    (_ : distancesWords) = words distancesRow
+    time = read $ concat timesWords
+    distance = read $ concat distancesWords
 
 readRaces :: String -> [Race]
 readRaces input = zipWith (curry intoRace) times distances
